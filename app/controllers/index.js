c6b3f9e45c9860b26fa5d637d57dc7480d1e1ecf
Abhinav Garg <abhinav.g@kiprosh.com>
2018-11-14 10:00:10 +0530

    computed value

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,10 +2,6 @@ import { computed, observer } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  isDisabled: true,
   emailAddress: '',
-
-  isDisabled: computed('emailAddress', function() {
-    return this.get('emailAddress') === '';
-  })
+  isDisabled: computed.empty('emailAddress')
 });
